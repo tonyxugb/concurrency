aeeeb4fcba2e39e58ed272fff4005b8e13e44860
tonyxugb <718521330@qq.com>
2016-12-21 00:08:49 +0800

    Initial commit

---
+++ src/main/java/com/tony/basic/Priority.java
@@ -0,0 +1,36 @@
+package com.tony.basic;
+
+/**
+ * Created by Administrator on 2016/12/20.
+ */
+public class Priority {
+
+    //为什么声明为volatile?
+    private static volatile boolean notStart = true;
+    private static volatile boolean notEnd = true;
+
+    public static void main(String[] args) throws Exception{
+
+
+
+    }
+
+
+    class Job implements Runnable{
+        private int priority;
+        private long jobCount;
+        public Job(int priority){
+            this.priority = priority;
+        }
+        public void run() {
+            while (notStart){
+                Thread.yield();
+            }
+            while (notEnd){
+                Thread.yield();
+                jobCount++;
+            }
+        }
+    }
+
+}
