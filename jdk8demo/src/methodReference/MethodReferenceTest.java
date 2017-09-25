efd0c6633a2d9f637dcb97ad4e7278f472fedf9c
xugebing <xugebing@jd.com>
2017-09-25 15:07:59 +0800

    方法引用

--- jdk8demo/src/methodReference/MethodReferenceTest.java
+++ jdk8demo/src/methodReference/MethodReferenceTest.java
@@ -9,13 +9,14 @@ public class MethodReferenceTest {
 
         String inStr = "lambda add power to Java";
         //MyStringOps::strReverse 相当于实现了接口方法func() ，并在接口方法func()中作了MyStringOps.strReverse()操作
-        String outStr = stringOp(MyStringOps::strReverse, inStr);
+        StringFunc sf = MyStringOps::strReverse;
+
+        String outStr = stringOp(sf, inStr);
         System.out.println("Original string: " + inStr);
         System.out.println("String reserved: " + outStr);
-
-
     }
 
+    //行为参数化
     public static String stringOp(StringFunc sf, String s) {
         return sf.func(s);
     }
